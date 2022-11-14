d30ce9efbbcb12f913e4bc2e3fd700f6499a6c7a
Rafał Rębilas <rafal.rebilas@comarch.com>
2022-11-14 16:43:36 +0100

    API call

--- app/src/main/java/pl/rrebilas/airquality/di/AirQualityProvider.kt
+++ app/src/main/java/pl/rrebilas/airquality/di/AirQualityProvider.kt
@@ -4,16 +4,54 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import pl.rrebilas.airquality.data.AirlyStationsDataSource
 import pl.rrebilas.airquality.di.entity.RemoteStationsRepository
-import pl.rrebilas.airquality.di.logic.FakeRemoteStationsRepository
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class AirQualityProvider {
+object AirQualityProvider {
     @Provides
     @Singleton
-    fun provideRemoteStationsRepository(): RemoteStationsRepository {
-        return FakeRemoteStationsRepository()
+    fun provideRemoteStationsRepository(airlyService: AirlyStationsDataSource.AirlyService): RemoteStationsRepository {
+        return AirlyStationsDataSource(airlyService)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAirlyAuthOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().addInterceptor(AirlyAuthInterceptor()).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient
+    ): Retrofit {
+        return Retrofit
+            .Builder()
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(AirlyStationsDataSource.HOST)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAirlyService(retrofit: Retrofit): AirlyStationsDataSource.AirlyService {
+        return retrofit.create(AirlyStationsDataSource.AirlyService::class.java)
+    }
+}
+
+class AirlyAuthInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val requestBuilder = chain.request().newBuilder()
+        requestBuilder.addHeader("apikey", "8OgIYZzbQUxKDcXLbZ0VHF6ZR9vQqkfg")
+        return chain.proceed(requestBuilder.build())
     }
 }

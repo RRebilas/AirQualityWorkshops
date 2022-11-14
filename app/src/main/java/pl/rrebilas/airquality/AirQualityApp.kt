3c178bdbb0111981ea4bfc4247a2d1e022572290
Rafał Rębilas <rafal.rebilas@comarch.com>
2022-11-14 12:22:47 +0100

    DI hilt

---
+++ app/src/main/java/pl/rrebilas/airquality/AirQualityApp.kt
@@ -0,0 +1,8 @@
+package pl.rrebilas.airquality
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AirQualityApp : Application() {
+}
---
+++ app/src/main/java/pl/rrebilas/airquality/di/AirQualityProvider.kt
@@ -0,0 +1,9 @@
+package pl.rrebilas.airquality.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class AirQualityProvider {}
